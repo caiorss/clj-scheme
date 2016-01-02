8d80774d0ab4215d4b2ff3881e986a883c5668d6
CaioRS <caiorss.rodrigues@gmail.com>
2016-01-02 06:52:11 -0300

    Project directory tree changed

--- gambcini.scm
+++ gambcini.scm
@@ -1,15 +1,14 @@
 
 (define System "Linux")
 
-(include "/home/tux/PycharmProjects/cljscheme/smacros.ss")
-(include "/home/tux/PycharmProjects/cljscheme/cljcore.scm")
+(include "/home/tux/PycharmProjects/cljscheme/core/smacros.ss")
+(include "/home/tux/PycharmProjects/cljscheme/core/cljcore.scm")
 ;;(include "/home/tux/PycharmProjects/cljscheme/cljmacros.scm")
-(include "/home/tux/PycharmProjects/cljscheme/system.scm")
+(include "/home/tux/PycharmProjects/cljscheme/core/system.scm")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(include "/home/tux/PycharmProjects/cljscheme/format.scm")
-(include "/home/tux/PycharmProjects/cljscheme/pregexp.scm" )
+(include "/home/tux/PycharmProjects/cljscheme/core/format.scm")
+(include "/home/tux/PycharmProjects/cljscheme/core/pregexp.scm" )
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
